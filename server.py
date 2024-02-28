4b50d582b73d6262d3f806c06fdb6f734555419e
Jorbon <ben.a.phillips@outlook.com>
2024-02-28 08:28:48 -0600

    semi working sync

--- server.py
+++ server.py
@@ -10,27 +10,60 @@ connections = []
 while True:
 	try:
 		conn, addr = s.accept()
-		print(f"Connected to {addr}")
-		connections.append(conn)
+		connections.append([conn, addr, True, []])
+		print(f"Connected to {addr}, {len(connections)} connections in total")
 	except BlockingIOError:
 		pass
 	
-	for conn in connections:
-		try:
-			data = conn.recv(256)
-			if data:
-				print(data)
-		except ConnectionResetError:
-			print(f"{conn.getpeername()} disconnected")
-			connections.remove(conn)
+	for i in range(len(connections)):
+		if connections[i][2] == False:
 			continue
-		except BlockingIOError:
-			pass
 		
 		try:
-			conn.sendall(b"ping from server")
-		except ConnectionResetError:
-			print(f"{conn.getpeername()} disconnected")
-			connections.remove(conn)
+			while True:
+				try:
+					connections[i][3].append(connections[i][0].recv(1))
+				except (ConnectionResetError, ConnectionAbortedError):
+					connections[i][2] = False
+					break
+				except BlockingIOError:
+					break
+			
+			if connections[i][2] == False:
+				continue
+			
+			data_len = len(connections[i][3]) - 2
+			if data_len >= 0:
+				message_len = int.from_bytes(b"".join(connections[i][3][:2]), "little")
+				if data_len >= message_len:
+					
+					data = b"".join(connections[i][3][2:])
+					
+					if data == b"exit":
+						connections[i][2] = False
+						continue
+					
+					connections[i][3] = connections[i][3][message_len + 2:]
+					print(f"Sending {data}")
+					
+					for j in range(len(connections)):
+						if i != j and connections[j][2] == True:
+							try:
+								connections[j][0].sendall(data)
+							except (ConnectionResetError, ConnectionAbortedError):
+								connections[j][2] = False
+								continue
+							except BlockingIOError:
+								pass
+			
+		except (ConnectionResetError, ConnectionAbortedError):
+			connections[i][2] = False
 			continue
+	
+	for i in range(len(connections))[::-1]:
+		if connections[i][2] == False:
+			connections[i][0].close()
+			print(f"{connections[i][1]} disconnected, {len(connections) - 1} connections in total")
+			connections.pop(i)
+			break
 	
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-use libmpv::{events::{Event, PropertyData}, Format};
-use std::io::{Read, Write};
+use libmpv::{events::{Event, PropertyData}, FileState, Format};
+use std::{io::{Read, Write}, net::TcpStream};
 
 extern crate libmpv;
+
+fn send(stream: &mut TcpStream, code: u8, data: &[u8]) {
+	let size = (data.len() + 1) as u16;
+	stream.write_all(&[&[size as u8, (size >> 8) as u8, code], data].concat()).unwrap()
+}
 
 fn main() {
 	let hostname = "localhost"; // "jorbonvm.centralus.cloudapp.azure.com";
 	let address = dns_lookup::lookup_host(hostname).unwrap();
-	let mut stream = std::net::TcpStream::connect((address[1], 7777u16)).unwrap();
-	
-	stream.write("test string".as_bytes()).unwrap();
-	
-	let mut buf = [0u8; 256];
-	let n = stream.read(&mut buf).unwrap();
-	println!("{n}: '{}'", String::from_utf8(buf.to_vec()).unwrap());
-	
-	return;
+	let mut stream = TcpStream::connect((address[1], 7777u16)).unwrap();
+	//stream.set_nodelay(true).unwrap();
+	stream.set_nonblocking(true).unwrap();
 	
 	let mpv = libmpv::Mpv::new().unwrap();
 	
@@ -32,24 +31,41 @@ fn main() {
 	let mut seeking = false;
 	let mut update_pos = false;
 	
+	let mut buf = [0u8; 256];
+	
 	loop {
-		if let Some(event) = events.wait_event(60.0) {
+		if let Ok(n) = stream.read(&mut buf) {
+			if n > 0 {
+				match buf[0] as char {
+					'p' => mpv.set_property("pause", match buf[1] { 0 => false, _ => true }).unwrap_or(()),
+					't' => mpv.set_property("playback-time", f64::from_le_bytes([buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7], buf[8]])).unwrap_or(()),
+					'f' => mpv.playlist_load_files(&[(&String::from_utf8(buf.split_at(1).1.to_vec()).unwrap(), FileState::Replace, None)]).unwrap_or(()),
+					_ => ()
+				}
+			}
+		}
+		
+		
+		if let Some(event) = events.wait_event(0.0) {
 			match event.unwrap() {
-				Event::PropertyChange { name: "pause", change: PropertyData::Flag(paused), .. } => println!("paused: {paused}"),
+				Event::PropertyChange { name: "pause", change: PropertyData::Flag(paused), .. } => send(&mut stream, 'p' as u8, &[match paused { true => 1, false => 0 }]),
 				Event::PropertyChange { name: "seeking", change: PropertyData::Flag(s), .. } => {
 					seeking = s;
 					if !s {
 						update_pos = true;
 					}
 				}
-				Event::PropertyChange { name: "path", change: PropertyData::Str(path), .. } => println!("{path}"),
+				Event::PropertyChange { name: "path", change: PropertyData::Str(path), .. } => send(&mut stream, 'f' as u8, &path.as_bytes()),
 				Event::PropertyChange { name: "playback-time", change: PropertyData::Double(time), .. } => {
 					if seeking || update_pos {
-						println!("Time: {time}s");
+						send(&mut stream, 't' as u8, &f64::to_le_bytes(time));
 						update_pos = false;
 					}
 				}
-				Event::Shutdown => break,
+				Event::Shutdown => {
+					stream.write_all(&[0, 4, 'e' as u8, 'x' as u8, 'i' as u8, 't' as u8]).unwrap();
+					break;
+				}
 				_ => {}
 			}
 		}
